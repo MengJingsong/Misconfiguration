6bd40cd69f42628d48b66835156c83dffaf3f903
JingsongMeng <mengjs.92@gmail.com>
2026-04-16 01:19:05 -0400

    update tmux.conf

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install htop
 
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-tmux source-file $SCRIPT_DIR/.tmux.conf
+cp $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
 
 git config --global core.editor "vim"
 git config --global user.email "mengjs.92@gmail.com"
