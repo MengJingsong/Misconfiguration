c06e007f84cad4901703c37f1c5b0da495c8850a
JingsongMeng <mengjs.92@gmail.com>
2026-04-16 01:02:07 -0400

    remove ssh-keygen from init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 
 sudo apt-get -y install vim
 sudo apt-get -y install tmux
-sudo apt-get -y htop
+sudo apt-get -y install htop
 
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
@@ -13,5 +13,3 @@ git config --global user.email "mengjs.92@gmail.com"
 git config --global user.name "JingsongMeng"
 
 sudo timedatectl set-timezone America/New_York
-
-ssh-keygen -t ed25519 -C "meng.479@osu.edu" -f ~/.ssh/id_ed25519 -N ""
